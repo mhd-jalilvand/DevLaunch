b78c787b142fbb56211fd883dd1691ed50472cd3
Mehdi <mhd.jalilvand@gmail.com>
2023-06-15 00:41:02 +0330

    allow multiple commands to run

--- windows/shell.py
+++ windows/shell.py
@@ -3,8 +3,8 @@ import tkinter as tk
 from tkinter import scrolledtext, Label, Entry, Button
 
 class Shell:
-    def __init__(self, command, use_sudo=False):
-        self.command = command
+    def __init__(self, commands, use_sudo=False):
+        self.commands = commands if isinstance(commands, list) else [commands]
         self.use_sudo = use_sudo
 
         # Create the shell window
@@ -33,29 +33,39 @@ class Shell:
         # Get the password if sudo is enabled
         password = self.password_entry.get() if self.use_sudo else None
 
-        # Execute the command and capture the output
-        if self.use_sudo and password:
-            command = f"echo {password} | sudo -S {self.command}"
-        else:
-            command = self.command
+        # Execute each command in the list
+        for i, command in enumerate(self.commands):
+            is_last_item = (i == len(self.commands) - 1)
+            self.output_text.insert(tk.END, "#" if self.use_sudo else "$" + command + "\n")
 
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            # Prepare the command with sudo if needed
+            if self.use_sudo and password:
+                command = f"echo {password} | sudo -S {command}"
 
-        # Read the output in real-time and display it in the text widget
-        while True:
-            output = process.stdout.readline().decode('utf-8')
-            if output == '' and process.poll() is not None:
+            process = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                shell=True
+            )
+            # Read the output in real-time and display it in the text widget
+            while True:
+                output = process.stdout.readline().decode('utf-8')
+                if output == '' and process.poll() is not None:
+                    break
+                self.output_text.insert(tk.END, output)
+                self.output_text.see(tk.END)
+                self.output_text.update_idletasks()
+
+            # Wait for the process to finish and retrieve the return code
+            return_code = process.wait()
+
+            # Display the exit status in the text widget
+            if(return_code !=0 or is_last_item):
+                self.output_text.insert(tk.END, f"\n\nCommand exited with status {return_code}\n\n")
+                self.output_text.see(tk.END)
+            if(return_code != 0):
                 break
-            self.output_text.insert(tk.END, output)
-            self.output_text.see(tk.END)
-            self.output_text.update_idletasks()
-
-        # Wait for the process to finish and retrieve the return code
-        return_code = process.wait()
-
-        # Display the exit status in the text widget
-        self.output_text.insert(tk.END, f"\n\nCommand exited with status {return_code}")
-        self.output_text.see(tk.END)
 
         # Disable text widget editing
         self.output_text.configure(state=tk.DISABLED)
